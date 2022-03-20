00ab9bd4971fc8e8969eae34280c4229fa4c671a
Aman Asati <amanasati86@gmail.com>
2022-03-20 21:11:10 +0530

    added-insert-into-table

---
+++ 03-insert-users-data.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO users (full_name, yearly_salary, current_status) 
+-- VALUES('Aman Asati', 600000, 'self-employed');
+
+-- INSERT INTO users (full_name, yearly_salary, current_status) 
+-- VALUES('Madhur Asati', 1500000, 'employed');
+
+INSERT INTO users (full_name, yearly_salary, current_status) 
+VALUES('Michel Smith', 0, 'unemployed');
