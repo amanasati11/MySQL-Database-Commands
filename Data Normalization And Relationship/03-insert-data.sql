26c0acf1298877cdfa3be59ff603709f6c00033e
Aman Asati <amanasati86@gmail.com>
2022-03-23 11:54:01 +0530

    'added-normalization-realationship'

---
+++ Data Normalization And Relationship/03-insert-data.sql
@@ -0,0 +1,16 @@
+-- INSERT INTO cities(name)
+-- VALUES('Berlin'), ('New York'), ('London');
+
+-- INSERT INTO addresses(street, house_number, city_id)
+-- VALUES
+--     ('Teststreet', '8A', 3), 
+--     ('Somestreet', '10', 1),
+--     ('Teststreet', '1A', 3),
+--     ('Mystreet', '101', 2);
+
+INSERT INTO users(first_name, last_name, email, address_id)
+VALUES
+    ('Max', 'Schwarz', 'max@test.com', 2),
+    ('Aman', 'Asati', 'aman@test.com', 4),
+    ('Madhur', 'Asati', 'madh@test.com', 1),
+    ('Ramprakash', 'Shah', 'ram@test.com', 3);
